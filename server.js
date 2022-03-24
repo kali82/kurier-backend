ed2fef3019b0e73696b51884f7039b78210eab76
mkaliszewski <mkaliszewski@ptc.com>
2022-03-24 16:49:42 +0100

    mongo uri string8

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const app = require('./app');
 const logger = require('./logger');
 
-const port = 8080;
-app.set('port', 4000);
+const port = process.env.PORT || 4000;
+app.set('port', port);
 logger.info(port)
 logger.info('8080')
-app.listen('4000');
+logger.info(process.env.PORT)
+app.listen(process.env.PORT || 4000);
