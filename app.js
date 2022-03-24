a479800de7d0744a6f5facc53b6b180d84beca4a
mkaliszewski <mkaliszewski@ptc.com>
2022-03-24 19:16:51 +0100

    heroku cors15

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
